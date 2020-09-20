54e56bb3de5fcde5a68df6eb20ac8fb08724ea0a
Ratul3 <ratulrayhan201@gmail.com>
2020-09-20 10:28:50 +0600

    report generating

--- resources/views/t_salary/salary.blade.php
+++ resources/views/t_salary/salary.blade.php
@@ -7,6 +7,7 @@
 <body>
 
 	<fieldset>
+	<div id="ph">
 	<legend><h1>SALARY</h1></legend>
 
 	<table border="1">
@@ -22,17 +23,18 @@
 			<td>{{$users[$i]->id}}</td>
 			<td>{{$users[$i]->name}}</td>
 			<td>{{$users[$i]->salary}}</td>
-		
-			<td>
-				<a href="{{route('home.sedit', [$users[$i]->id])}}">Edit</a> |
-				<a href="{{route('home.sdelete', [$users[$i]->id])}}">Delete</a>
-			</td>
+			<td><a href="{{route('home.sedit', [$users[$i]->id])}}">Edit</a> | <a href="{{route('home.sdelete', [$users[$i]->id])}}">Delete</a></td>
 		</tr>
 	@endfor
 	</table>
+	</div>
 	<br/>
 	<a href="{{route('home.sinsert')}}">Insert</a> 
+	<a href="javascript:pdfToHTML()">Save as PDF</a><br/><br/>
 <a href="{{route('home.index')}}">Back</a>
 </fieldset>
 </body>
+<script src="/js/jquery-2.1.3.js"></script>
+<script src="/js/pdfFromHTML.js"></script>
+<script src="/js/jspdf.js"></script>
 </html>
--- resources/views/t_schedule/schedule.blade.php
+++ resources/views/t_schedule/schedule.blade.php
@@ -7,6 +7,7 @@
 <body>
 
 	<fieldset>
+	<div id="ph">
 	<legend><h1>SCHEDULE</h1></legend>
 
 	<table border="1">
@@ -24,17 +25,18 @@
 			<td>{{$users[$i]->name}}</td>
 			<td>{{$users[$i]->day}}</td>
 			<td>{{$users[$i]->time}}</td>
-		
-			<td>
-				<a href="{{route('home.scheduleedit', [$users[$i]->id])}}">Edit</a> |
-				<a href="{{route('home.scheduledelete', [$users[$i]->id])}}">Delete</a>
-			</td>
+			<td><a href="{{route('home.scheduleedit', [$users[$i]->id])}}">Edit</a> | <a href="{{route('home.scheduledelete', [$users[$i]->id])}}">Delete</a></td>
 		</tr>
 	@endfor
 	</table>
+	</div>
 	<br/>
 	<a href="{{route('home.scheduleinsert')}}">Insert</a> 
+	<a href="javascript:pdfToHTML()">Save as PDF</a><br/><br/>
 <a href="{{route('home.index')}}">Back</a>
 </fieldset>
 </body>
+<script src="/js/jquery-2.1.3.js"></script>
+<script src="/js/pdfFromHTML.js"></script>
+<script src="/js/jspdf.js"></script>
 </html>
